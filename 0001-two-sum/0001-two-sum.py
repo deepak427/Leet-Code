58a83e19d33c647642ca24443f7b36615d926216
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-07-02 09:54:07 +0530

    Time: 59 ms (65%), Space: 17.7 MB (49%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dictMap = {}
         for i in range(len(nums)):
-            if (target-nums[i]) in nums:
-                if i != nums.index(target-nums[i]):
-                    return [i, nums.index(target-nums[i])]
+            if (target-nums[i]) in dictMap.keys():
+                if i != dictMap.get(target-nums[i]):
+                    return [i, dictMap.get(target-nums[i])]
+            else:
+                dictMap[nums[i]] = i
         
         

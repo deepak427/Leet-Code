19fc23c681e778e615e54db3e5cfd2060655b228
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-07-10 11:50:56 +0530

    Time: 6 ms (88.57%), Space: 14.9 MB (10.8%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -9,17 +9,12 @@ public:
         for(int i = 0; i< nums.size(); ++i){
             if(m_nums.find(target - nums[i]) != m_nums.end()){
                 if( i != m_nums.find(target - nums[i])->second){
-                    cout << i << endl;
-                    vector<int> result;
-                    result.push_back(i);
-                    result.push_back(m_nums.find(target - nums[i])->second);
-                    return result;                    
+                    return {i, m_nums.find(target - nums[i])->second};                    
                 }
             }
             m_nums.erase(nums[i]);
         }
-        vector<int> result;
-        return result;
+        return {0,0};
 
     }
 };

94d70c218adab7987fea49baa888c26457c1e703
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-07-13 21:28:51 +0530

    Time: 4 ms (82.22%), Space: 9.3 MB (41.09%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -9,8 +9,16 @@ public:
                 start++;
             }
 
+            if(start >= end){
+                break;
+            }
+
             while(start < end && (s[end] != 'a' && s[end] != 'e' && s[end] != 'i'  && s[end] != 'o' && s[end] != 'u' && s[end] != 'A' && s[end] != 'E' && s[end] != 'I'  && s[end] != 'O' && s[end] != 'U' )){
                 end--;
+            }
+
+            if(start >= end){
+                break;
             }
 
             swap(s[start], s[end]);

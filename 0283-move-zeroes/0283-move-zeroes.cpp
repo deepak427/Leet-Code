52ba346f0bddd652ce9ef8bf70d75f0e016a6d65
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-07-15 15:38:00 +0530

    Time: 25 ms (11.87%), Space: 22 MB (10.94%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,10 +3,10 @@ public:
     void moveZeroes(vector<int>& nums) {
         int a = nums.size();
         for(int i = 0; i< nums.size();++i){
-            if(i ==a ){
-                break;
-            }
             if(nums[i] == 0){
+                if(i ==a ){
+                    break;
+                }
                 nums.erase(nums.begin() + i);
                 nums.push_back(0);
                 i--;

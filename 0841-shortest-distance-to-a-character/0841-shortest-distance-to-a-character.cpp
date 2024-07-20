418283c4c5712158d588579cecf683706512a144
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-07-20 13:42:16 +0530

    Time: 4 ms (44.84%), Space: 8.3 MB (6.45%) - LeetHub

--- 0841-shortest-distance-to-a-character/0841-shortest-distance-to-a-character.cpp
+++ 0841-shortest-distance-to-a-character/0841-shortest-distance-to-a-character.cpp
@@ -1,38 +1,16 @@
 class Solution {
 public:
-    vector<int> shortestToChar(string &s, char &c) {
-
-        vector<int> answer(s.size());
-
-        int start = 0;
-
-        size_t found = s.find(c, start);
-        if (found != string::npos){
-            for(int i = start; i <= found; ++i){
-                answer[i] = found-i;
-            }
-            start = found+1;
+        vector<int> shortestToChar(string S, char C) {
+        int n = S.size(), pos = -n;
+        vector<int> res(n, n);
+        for (int i = 0; i < n; ++i) {
+            if (S[i] == C) pos = i;
+            res[i] = i - pos;
         }
-
-        while(start < s.size()){
-            found = s.find(c, start);
-            if (found != string::npos){
-                for(int i = start; i <= found; ++i){
-                    if((found-i) < (i-(start-1))){
-                        answer[i] = found-i;
-                    }else{
-                        answer[i] = i-(start-1);
-                    }
-                }
-                start = found+1;
-            }else{
-                 for(int i = start; i < s.size(); ++i){
-                    answer[i] = i-(start-1);
-                }     
-                break;          
-            }
+        for (int i = pos - 1; i >= 0; --i) {
+            if (S[i] == C)  pos = i;
+            res[i] = min(res[i], pos - i);
         }
-
-        return answer;
+        return res;
     }
 };

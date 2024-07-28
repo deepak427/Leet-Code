ddc230dd4ca9c64603c81a42efe64f61f298019f
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-07-28 15:43:25 +0530

    Time: 164 ms (74.17%), Space: 96.7 MB (79.29%) - LeetHub

---
+++ 3235-minimum-cost-to-convert-string-i/3235-minimum-cost-to-convert-string-i.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
+        int noOfChars = 26;
+        #define INF 1e9
+        vector<vector<int>> graph(26, vector<int>(26,INF));
+
+        if(source == target){
+            return 0;
+        }
+
+        for(int i = 0;i < 26; ++i){
+            graph[i][i] = 0;
+        }
+
+        for(int i =0; i< original.size(); ++i){
+            int intValueOrignal = original[i] - 'a';
+            int intValueChanged = changed[i] - 'a';
+
+            graph[intValueOrignal][intValueChanged] = min(cost[i],graph[intValueOrignal][intValueChanged]);
+        }
+
+        for(int i = 0; i < noOfChars; ++i){
+            for(int j = 0; j < noOfChars; ++j){
+                for(int k =0; k < noOfChars; ++k){
+                    int intermediateCost = graph[j][i] + graph[i][k];
+                    if(intermediateCost < graph[j][k]){
+                        graph[j][k] = intermediateCost;
+                    }
+                }
+            }
+        }
+
+        long long costResult = 0;
+
+        for(int i = 0; i < source.size(); ++i){
+            int intValue = source[i] - 'a';
+            int intValueTarget = target[i] - 'a';
+            if(intValue != intValueTarget){
+                if(graph[intValue][intValueTarget] != INF){
+                    costResult = costResult + graph[intValue][intValueTarget];
+                }else {
+                    return -1;
+                }
+            }
+        }
+
+        return costResult;
+
+    }
+};

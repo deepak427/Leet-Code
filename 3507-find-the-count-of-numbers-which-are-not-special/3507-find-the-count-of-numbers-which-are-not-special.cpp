c63e62f48753ab156c3cb1b27003d2d52e0605e7
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-07-29 22:38:30 +0530

    Time: 71 ms (69.23%), Space: 9.5 MB (92.31%) - LeetHub

--- 3507-find-the-count-of-numbers-which-are-not-special/3507-find-the-count-of-numbers-which-are-not-special.cpp
+++ 3507-find-the-count-of-numbers-which-are-not-special/3507-find-the-count-of-numbers-which-are-not-special.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int nonSpecialCount(int l, int r) {
-        int n = sqrt(r);
-
-        vector<bool> v(n+1, 1);
+        vector<bool> v(sqrt(r)+1, 1);
         v[0] = 0;
         v[1] = 0;
 
         int result = 0;
 
-        for(int i =2; i <= n; ++i){
+        for(int i =2; i <= sqrt(r); ++i){
             if(v[i]){
                 int squared = i*i;
                 if((squared >= l) && (squared <= r)){
                     result++;
                 }
 
-                for(int j = i*i; j <=n; j+=i){
+                for(int j = i*i; j <=sqrt(r); j+=i){
                     v[j] = 0;
                 }
             }

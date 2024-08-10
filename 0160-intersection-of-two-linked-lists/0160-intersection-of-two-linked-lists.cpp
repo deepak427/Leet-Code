91e2eefc4a67326eddaa7e941103dc966b255ca3
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-08-10 10:43:18 +0530

    Time: 37 ms (66.61%), Space: 17.4 MB (23.58%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -1,48 +1,22 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        int lengthA = 0;
-        int lengthB = 0;
-        ListNode *headACopy = headA;
-        ListNode *headBCopy = headB;
-
-
-        while(headA != nullptr){
-            headA = headA->next;
-            lengthA++;      
-        }
-
-        while( headB != nullptr){     
-            headB = headB->next;    
-            lengthB++;  
-        }   
-
-        if(lengthA < lengthB){
-            for(int i = 0; i < (lengthB-lengthA); ++i){
-                headBCopy=headBCopy->next;
-            }
-        }else {
-            for(int i = 0; i < (lengthA-lengthB); ++i){
-                headACopy=headACopy->next;
-            }            
-        }
-
-        while(headACopy != nullptr && headBCopy != nullptr){
-            if(headACopy==headBCopy){
-                return headACopy;
-            }
-            headACopy=headACopy->next;
-            headBCopy=headBCopy->next;
-        }
-
-        return nullptr;
-    }
+	ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+		ListNode *ptr1 = headA;
+		ListNode *ptr2 = headB;
+		while(ptr1 != ptr2){
+			if(ptr1 == NULL){
+				ptr1 = headB;
+			}
+			else{
+				ptr1 = ptr1 -> next;
+			}
+			if(ptr2 == NULL){
+				ptr2 = headA;
+			}
+			else{
+				ptr2 = ptr2 -> next;
+			}
+		}
+		return ptr1;
+	}
 };

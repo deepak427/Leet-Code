cf687407124a54748231c489c6f34492d0ef5967
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-10-10 11:00:05 +0530

    Time: 66 ms (68.96%), Space: 45.4 MB (61.98%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -5,18 +5,15 @@ public:
         int result = 0;
         unordered_map<int, int> sum_m;
 
-        // Initialize map with 0 sum to handle cases where subarray starts from index 0
         sum_m[0] = 1; 
 
         for (int i = 0; i < nums.size(); ++i) {
             sum += nums[i];
 
-            // Check if there is a subarray that sums to k
             if (sum_m.find(sum - k) != sum_m.end()) {
-                result += sum_m[sum - k]; // Add the frequency of the sum found
+                result += sum_m[sum - k]; 
             }
 
-            // Increment the frequency of the current sum
             sum_m[sum]++;
         }
 
